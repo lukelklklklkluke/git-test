71bc7f40e9d047005b8b3991f649f96cd87671bc
luke <207350732@qq.com>
2022-01-18 17:11:02 +0800

    first commit

---
+++ src/main/java/com/lk/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lk.git;
+
+/**
+ * @author lkstart
+ * @create 2022-01-18-16:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
